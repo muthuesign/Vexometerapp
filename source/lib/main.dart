beb285bf0ae6197a408594b3f659704193532a7a
Shubham Gupta <Shubhamg836@gmail.com>
2021-05-13 14:26:49 +0530

    Changes

--- source/lib/main.dart
+++ source/lib/main.dart
@@ -1,12 +1,152 @@
+import 'dart:io';
+
+import 'package:device_info/device_info.dart';
 import 'package:flutter/material.dart';
 import 'package:vaxometer/src/home.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
+
+
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState  extends State<MyApp>  {
   // This widget is the root of your application.
+  String _debugLabelString = "";
+  String _emailAddress;
+  String _externalUserId;
+  bool _enableConsentButton = false;
+
+  // CHANGE THIS parameter to true if you want to test GDPR privacy consent
+  bool _requireConsent = true;
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  Future<void> initPlatformState() async {
+    if (!mounted) return;
+    OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
+    OneSignal.shared.init(
+        "6eba4686-2cd0-4a2e-a374-e04df273d17d",
+        iOSSettings: {
+          OSiOSSettings.autoPrompt: false,
+          OSiOSSettings.inAppLaunchUrl: false
+        }
+    );
+//    var myCustomUniqueUserId = await _getId();
+//    OneSignal.shared.setExternalUserId(myCustomUniqueUserId);
+
+    var status = await OneSignal.shared.getPermissionSubscriptionState();
+    String onesignalUserId = status.subscriptionStatus.userId; // Store this in MongoDB
+
+    OneSignal.shared.setInFocusDisplayType(OSNotificationDisplayType.notification);
+    await OneSignal.shared.promptUserForPushNotificationPermission(fallbackToSettings: true);
+
+    OneSignal.shared.setNotificationReceivedHandler((OSNotification notification) {
+      // will be called whenever a notification is received
+    });
+
+    OneSignal.shared.setNotificationOpenedHandler((OSNotificationOpenedResult result) {
+      // will be called whenever a notification is opened/button pressed.
+    });
+
+    OneSignal.shared.setPermissionObserver((OSPermissionStateChanges changes) {
+      // will be called whenever the permission changes
+      // (ie. user taps Allow on the permission prompt in iOS)
+    });
+
+    OneSignal.shared.setSubscriptionObserver((OSSubscriptionStateChanges changes) {
+      // will be called whenever the subscription changes
+      //(ie. user gets registered with OneSignal and gets a user ID)
+    });
+
+    OneSignal.shared.setEmailSubscriptionObserver((OSEmailSubscriptionStateChanges emailChanges) {
+      // will be called whenever then user's email subscription changes
+      // (ie. OneSignal.setEmail(email) is called and the user gets registered
+    });
+
+    /*
+    OneSignal.shared.setRequiresUserPrivacyConsent(_requireConsent);
+
+
+    var settings = {
+      OSiOSSettings.autoPrompt: false,
+      OSiOSSettings.promptBeforeOpeningPushUrl: true
+    };
+
+    OneSignal.shared.setNotificationReceivedHandler((OSNotification notification) {
+      this.setState(() {
+        _debugLabelString =
+        "Received notification: \n${notification.jsonRepresentation().replaceAll("\\n", "\n")}";
+      });
+    });
+
+    OneSignal.shared
+        .setNotificationOpenedHandler((OSNotificationOpenedResult result) {
+      this.setState(() {
+        _debugLabelString =
+        "Opened notification: \n${result.notification.jsonRepresentation().replaceAll("\\n", "\n")}";
+      });
+    });
+
+    OneSignal.shared
+        .setInAppMessageClickedHandler((OSInAppMessageAction action) {
+      this.setState(() {
+        _debugLabelString =
+        "In App Message Clicked: \n${action.jsonRepresentation().replaceAll("\\n", "\n")}";
+      });
+    });
+
+    OneSignal.shared
+        .setSubscriptionObserver((OSSubscriptionStateChanges changes) {
+      print("SUBSCRIPTION STATE CHANGED: ${changes.jsonRepresentation()}");
+    });
+
+    OneSignal.shared.setPermissionObserver((OSPermissionStateChanges changes) {
+      print("PERMISSION STATE CHANGED: ${changes.jsonRepresentation()}");
+    });
+
+    OneSignal.shared.setEmailSubscriptionObserver(
+            (OSEmailSubscriptionStateChanges changes) {
+          print("EMAIL SUBSCRIPTION STATE CHANGED ${changes.jsonRepresentation()}");
+        });
+
+    // NOTE: Replace with your own app ID from https://www.onesignal.com
+
+    await OneSignal.shared
+        .init("6eba4686-2cd0-4a2e-a374-e04df273d17d", iOSSettings: settings);
+
+    OneSignal.shared
+        .setInFocusDisplayType(OSNotificationDisplayType.notification);
+
+    bool requiresConsent = await OneSignal.shared.requiresUserPrivacyConsent();
+
+    this.setState(() {
+      _enableConsentButton = requiresConsent;
+    });
+    */
+
+  }
+
+  Future<String> _getId() async {
+    var deviceInfo = DeviceInfoPlugin();
+    if (Platform.isIOS) { // import 'dart:io'
+      var iosDeviceInfo = await deviceInfo.iosInfo;
+      return iosDeviceInfo.identifierForVendor; // unique ID on iOS
+    } else {
+      var androidDeviceInfo = await deviceInfo.androidInfo;
+      return androidDeviceInfo.androidId; // unique ID on Android
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -30,4 +170,6 @@ class MyApp extends StatelessWidget {
       home: Home(),
     );
   }
+
+
 }
