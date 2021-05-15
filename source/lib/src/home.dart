413a123e56af8c72c18c9942e687d518249de408
muthukrishnan_rangas <muthukrishnan_rangas@dell.com>
2021-05-15 20:59:17 +0530

    design changes

--- source/lib/src/home.dart
+++ source/lib/src/home.dart
@@ -15,6 +15,7 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   Future<List<VaccineCentre>> _vaccineCentres;
   String _pinCode;
+  var _vacCentrefilters = [true, true, true, true];
 
   @override
   void initState() {
@@ -27,6 +28,14 @@ class _HomeState extends State<Home> {
     var response = await HttpService.get<dynamic>("https://vaxometerindia.azurewebsites.net/api/v1/Vaxometer/Centers/pincode/" + _pinCode );
     var vaccCentres = List.generate(response.length,
         (i) => VaccineCentre.fromJson(response[i]));
+
+    // vaccCentres = vaccCentres.where((ele) => 
+    //   (!_vacCentrefilters[0] || (ele.sessions != null && ele.sessions.any((a) => a.min_age_limit <= 18))) 
+    //   && (!_vacCentrefilters[1] || (ele.sessions != null && ele.sessions.any((a) => a.min_age_limit <= 45))) 
+    //   && (!_vacCentrefilters[2] || ele.fee_type == 'Free')
+    //   &&  (!_vacCentrefilters[3] || ele.fee_type == 'Paid')
+    // );
+
     return vaccCentres;
   }
 
@@ -74,61 +83,67 @@ class _HomeState extends State<Home> {
     );
   }
   
-  bool isAge45 = true;
-  var filters = [true, true, true, true];
-   Widget buildBar(BuildContext context) {
+  
+  Widget buildBar(BuildContext context) {
     return new AppBar(
-        leadingWidth: 35.0,
-        leading: IconButton(iconSize: 20.0, icon: Icon(Icons.list, color: Colors.white), onPressed: () {
-            
-          }),
-        
-        bottom: PreferredSize(
-          preferredSize: Size.fromHeight(35.0),
-          child: Container(
-            height: 35.0,
-            padding: EdgeInsets.symmetric(vertical: 2.0),
-            color: Colors.white30,
-            child: ToggleButtons(
-                borderColor: Colors.blueGrey,
-                fillColor: Colors.white,
-                borderRadius: BorderRadius.circular(2.0),
-                selectedBorderColor: Colors.blue,
-                color: Colors.blueGrey,
-                children: <Widget>[
-                  Container(width: (MediaQuery.of(context).size.width - 5)/4, child:Text("18+", textAlign: TextAlign.center)),
-                  Container(width: (MediaQuery.of(context).size.width - 5)/4, child:Text("45+", textAlign: TextAlign.center)),
-                  Container(width: (MediaQuery.of(context).size.width - 5)/4, child:Text("Free", textAlign: TextAlign.center)),
-                  Container(width: (MediaQuery.of(context).size.width - 5)/4, child:Text("Paid", textAlign: TextAlign.center))
-                ],
-                onPressed: (int index) {
-                  setState(() {
-                    filters[index] = !filters[index];
-                  });
-                },
-                isSelected: filters
-              )
-          ),
-        ),
-        title: Container(
-          height: 25.0,
-          child: TextField(
-            decoration: InputDecoration(
-              hintStyle: TextStyle(color: Colors.white24),
-              contentPadding: EdgeInsets.all(5.0),
-              hintText: 'Search center',
-              isDense: true
+      leadingWidth: 35.0,
+      flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color.fromRGBO(55, 167, 148, 1), Color.fromRGBO(181, 220, 202, 1)],
+              ),
             ),
           ),
+      // leading: IconButton(iconSize: 20.0, icon: Icon(Icons.list, color: Colors.white), onPressed: () {
+          
+      //   }),
+      
+      bottom: PreferredSize(
+        preferredSize: Size.fromHeight(35.0),
+        child: Container(
+          height: 35.0,
+          padding: EdgeInsets.symmetric(vertical: 2.0),
+          color: Colors.white30,
+          child: ToggleButtons(
+              borderColor: Colors.blueGrey,
+              fillColor: Colors.white,
+              borderRadius: BorderRadius.circular(2.0),
+              selectedBorderColor: Colors.blue,
+              color: Colors.blueGrey,
+              children: <Widget>[
+                Container(width: (MediaQuery.of(context).size.width - 5)/4, child:Text("18+", textAlign: TextAlign.center)),
+                Container(width: (MediaQuery.of(context).size.width - 5)/4, child:Text("45+", textAlign: TextAlign.center)),
+                Container(width: (MediaQuery.of(context).size.width - 5)/4, child:Text("Free", textAlign: TextAlign.center)),
+                Container(width: (MediaQuery.of(context).size.width - 5)/4, child:Text("Paid", textAlign: TextAlign.center))
+              ],
+              onPressed: (int index) {
+                setState(() {
+                  _vacCentrefilters[index] = !_vacCentrefilters[index];
+                });
+              },
+              isSelected: _vacCentrefilters
+            )
         ),
-        actions: <Widget>[
-          new IconButton(iconSize: 20.0, icon: Icon(Icons.search, color: Colors.white), onPressed: () {
-            
-          },),
-          new IconButton(iconSize: 20.0, icon: Icon(Icons.filter_alt_outlined, color: Colors.white), onPressed: () {
-            
-          },)
-        ]
+      ),
+      title: Container(
+        height: 25.0,
+        child: TextField(
+          decoration: InputDecoration(
+            hintStyle: TextStyle(color: Colors.white24),
+            contentPadding: EdgeInsets.all(5.0),
+            hintText: 'Search center',
+            isDense: true
+          ),
+        ),
+      ),
+      actions: <Widget>[
+        new IconButton(iconSize: 20.0, icon: Icon(Icons.search, color: Colors.black), onPressed: () {
+          
+        })//,
+        // new IconButton(iconSize: 20.0, icon: Icon(Icons.filter_alt_outlined, color: Colors.white), onPressed: () {
+          
+        // },)
+      ]
     );
   }
 }
